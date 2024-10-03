4c59c9458fbcf36256d0a24bd24f1f6c1e1bc4c5
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-10-03 23:45:36 +0300

    mm

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,33 +1,36 @@
 export default class Currency {
   constructor(code, name) {
-    this.name = name;
-    this.code = code;
+    this.name = name; // Use setter for validation
+    this.code = code; // Use setter for validation
   }
 
+  // Getter for name
   get name() {
     return this._name;
   }
 
+  // Getter for code
   get code() {
     return this._code;
   }
 
+  // Setter for name
   set name(value) {
-    if (typeof value != 'string') {
+    if (typeof value !== 'string') {
       throw new TypeError('Name must be a string');
-    } else {
-      this._name = value;
     }
+    this._name = value;
   }
 
+  // Setter for code
   set code(value) {
-    if (typeof code != 'string') {
+    if (typeof value !== 'string') {
       throw new TypeError('Code must be a string');
-    } else {
-      this._code = value;
     }
+    this._code = value;
   }
 
+  // Method to display the full currency
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
