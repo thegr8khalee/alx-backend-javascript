71e0ab822b21f58f300ae69c8ac56708eeb1bef7
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-10-04 00:05:35 +0300

    mm

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -15,9 +15,10 @@ export class HolbertonClass {
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
+    // Add holbertonClass to constructor
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    this._holbertonClass = holbertonClass; // Use _holbertonClass
   }
 
   get fullName() {
@@ -25,24 +26,31 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this._holbertonClass;
+    return this._holbertonClass; // Return _holbertonClass, not holbertonClass
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - \
-  ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    // Use this instead of self
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
+// Instantiate HolbertonClass objects
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
+// Instantiate StudentHolberton objects
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
-
-export default listOfStudents;
+// Export the list of students
+export const listOfStudents = [
+  student1,
+  student2,
+  student3,
+  student4,
+  student5,
+];
