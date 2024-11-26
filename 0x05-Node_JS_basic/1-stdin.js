54d85113105e250e7890c2fa1c7b9b3800a71643
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-11-26 16:01:26 +0300

    mm

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+read.question('Welcome to Holberton School, what is your name? ', (name) => {
+  console.log('Your name is: ${name');
+  read.close();
+  console.log('This important software is now closing');
+});
