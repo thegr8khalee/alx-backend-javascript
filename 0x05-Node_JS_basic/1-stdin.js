da7971ceb6f315f9ba8487df99aa6367eb43beef
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-11-26 16:12:02 +0300

    mm

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
 process.stdout.write('Welcome to holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  process.stdout.write('Your name is: ${name}\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
