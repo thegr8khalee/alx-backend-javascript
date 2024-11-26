cbbb52752fbbc6bd2fb2024fa3a9d4389ecf218f
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-11-26 17:42:37 +0300

    mm

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,20 @@
-var app = require('http');
+const http = require('http');
 
-app
-  .createServer(function (req, res) {
-    res.write('Hello Holberton School!'); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(1245);
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
