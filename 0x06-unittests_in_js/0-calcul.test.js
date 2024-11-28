827d7a8259a145c48c85bc617ee4a1b6c8dacebb
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-11-28 15:45:30 +0300

    mm

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,31 @@
-const ass = require('assert');
+const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('Should return 4 when inputs 1.4 and 2.6', () => {
-    ass.strictEqual(calculateNumber(1.4, 2.6), 4);
+    assert.strictEqual(calculateNumber(1.4, 2.6), 4);
+  });
+  it('should return 6 when inputs are 2.5 and 2.5', () => {
+    assert.strictEqual(calculateNumber(2.5, 2.5), 6);
+  });
+
+  it('should return 0 when inputs are -0.4 and 0.4', () => {
+    assert.strictEqual(calculateNumber(-0.4, 0.4), 0);
+  });
+
+  it('should return -1 when inputs are -0.6 and -0.4', () => {
+    assert.strictEqual(calculateNumber(-0.6, -0.4), -1);
+  });
+
+  it('should return 10 when inputs are 9.7 and 0.3', () => {
+    assert.strictEqual(calculateNumber(9.7, 0.3), 10);
+  });
+
+  it('should handle large numbers correctly', () => {
+    assert.strictEqual(calculateNumber(12345.6, 54321.3), 66667);
+  });
+
+  it('should handle zero inputs correctly', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0);
   });
 });
